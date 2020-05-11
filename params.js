2eabff8dbc79de51ac70b8335c5c8325a36ac25a
Vanxi <1050132079@qq.com>
2020-05-11 21:13:48 +0800

    d

--- params.js
+++
@@ -1,9 +0,0 @@
-
-searchKeyowrds({
-    keywords,  //搜索的关键字，string or array
-    pathArr,
-    emitFile = true,
-    filename = "./_keywords.json",
-    validSuffix =[".js", ".jsx"],
-    exclude =["./node_modules"]
-})
